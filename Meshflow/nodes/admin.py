df4b9da93b4345f686f90253a4fc4cb007c983b2
Patrick Skillen <pskillen@gmail.com>
2025-04-22 13:16:04 +0100

    feat: admin pages for nodes, users

--- Meshflow/nodes/admin.py
+++ Meshflow/nodes/admin.py
@@ -1,5 +1,60 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from .models import MeshtasticNode
 
-admin.site.register(MeshtasticNode)
+
+@admin.register(MeshtasticNode)
+class MeshtasticNodeAdmin(admin.ModelAdmin):
+    list_display = (
+        "short_name",
+        "long_name",
+        "node_id_str",
+        "constellation",
+        "hw_model",
+        "sw_version",
+    )
+    list_filter = (
+        "constellation",
+        "hw_model",
+    )
+    search_fields = (
+        "short_name",
+        "long_name",
+        "node_id",
+        "mac_addr",
+        "hw_model",
+        "sw_version",
+    )
+    readonly_fields = (
+        "internal_id",
+        "node_id",
+        "node_id_str",
+        "mac_addr",
+        "public_key",
+    )
+    fieldsets = (
+        (None, {
+            "fields": (
+                "constellation",
+                "short_name",
+                "long_name",
+            )
+        }),
+        (_("Hardware Information"), {
+            "fields": (
+                "hw_model",
+                "sw_version",
+            )
+        }),
+        (_("Technical Details"), {
+            "fields": (
+                "node_id",
+                "node_id_str",
+                "mac_addr",
+                "public_key",
+            ),
+            "classes": ("collapse",),
+        }),
+    )
+
