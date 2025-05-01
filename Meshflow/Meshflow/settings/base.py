bbedf139315d45d3425b8d336d528693e220cdeb
Patrick Skillen <pskillen@gmail.com>
2025-05-01 21:58:14 +0100

    feat: add pagination to docs, allow setting page size

--- Meshflow/Meshflow/settings/base.py
+++ Meshflow/Meshflow/settings/base.py
@@ -160,8 +160,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "nodes.authentication.NodeAPIKeyAuthentication",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 50,
+    "DEFAULT_PAGINATION_CLASS": "Meshflow.paginator.PageSizePagination",
 }
 
 # JWT Settings
