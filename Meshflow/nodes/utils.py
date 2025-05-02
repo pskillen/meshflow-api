f66f47939158cb455853da64490717ea037828bf
Patrick Skillen <pskillen@gmail.com>
2025-05-02 22:37:21 +0100

    refactor: only use 2 words, 2 digits for claim key

--- Meshflow/nodes/utils.py
+++ Meshflow/nodes/utils.py
@@ -7,7 +7,7 @@ def generate_claim_key():
     mnemo = Mnemonic("english")
     # Generate 12-word phrase, then pick 3 random words from it for more entropy
     words = mnemo.generate(strength=128).split()
-    selected_words = random.sample(words, 3)
-    number = random.randint(100, 999)
+    selected_words = random.sample(words, 2)
+    number = random.randint(10, 99)
     # Join with spaces, append number
     return f"{' '.join(selected_words)} {number}"
