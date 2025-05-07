4776a2a2515a50ee8b23e1d7a972a982538f194f
Patrick Skillen <pskillen@gmail.com>
2025-05-07 09:30:45 +0100

    fix: disable unused URLs

--- Meshflow/users/urls.py
+++ Meshflow/users/urls.py
@@ -12,9 +12,9 @@ from users.social_auth import (
 urlpatterns = [
     # dj-rest-auth endpoints
     path("", include("dj_rest_auth.urls")),
-    path("registration/", include("dj_rest_auth.registration.urls")),
+    # path("registration/", include("dj_rest_auth.registration.urls")),
     # Social auth endpoints
-    path("accounts/", include("allauth.urls")),
+    # path("accounts/", include("allauth.urls")),
     path("social/google/", GoogleLoginRedirectView.as_view(), name="google_login"),
     path("social/google/callback/", GoogleCallbackRedirectView.as_view(), name="google_login_callback"),
     path("social/google/token/", GoogleLoginView.as_view(), name="google_token"),
