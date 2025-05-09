a6ed64d46ff2f59e9fc6b3d4242e4ad317764ba9
Patrick Skillen <pskillen@gmail.com>
2025-05-09 22:51:20 +0100

    feat: add a /stats/.../received/ endpoint

--- Meshflow/stats/urls.py
+++ Meshflow/stats/urls.py
@@ -6,5 +6,6 @@ app_name = "stats"
 
 urlpatterns = [
     path("nodes/<int:node_id>/packets/", views.node_packet_stats, name="node-packet-stats"),
+    path("nodes/<int:node_id>/received/", views.node_received_stats, name="node-received-stats"),
     path("global/", views.global_packet_stats, name="global-packet-stats"),
 ]
