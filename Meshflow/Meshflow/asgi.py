4d42f55835665c93f1b640273c4796e8939d1891
Patrick Skillen <pskillen@gmail.com>
2025-05-18 21:29:23 +0100

    fix: import dotenv no matter what entrypoint we use

--- Meshflow/Meshflow/asgi.py
+++ Meshflow/Meshflow/asgi.py
@@ -14,8 +14,11 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
+from dotenv import load_dotenv
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Meshflow.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Meshflow.settings.monolith")
+
+load_dotenv()
 
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
